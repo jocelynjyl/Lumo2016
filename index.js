99ecaaf5d2ebe13a6e74fcdbd017f30ba775ab94
Camellia Peng <camelliapeng02@gmail.com>
2016-09-17 20:07:10 -0700

    configured mongoose url

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 var express = require("express"),
     app = express(),
-    mongoose = require("mongoose");
+    mongoose = require("mongoose"),
+    path = require("path"),
+    jsonGetCall = require(path.join(__dirname, "lib", "fns", "jsonGetCall"));
 
     /*============ MONGOOSE ============*/
-    mongoose.connect("mongodb://localhost/lumohack");
+    var mongooseURL = process.env.MONGOLAB_URI || "mongodb://localhost/lumohack";
+    mongoose.connect(mongooseURL);
+
+    /*============ TEST API CALL ============*/
+    // jsonGetCall("https://clinicaltrialsapi.cancer.gov/v1/clinical-trial.json",
+    //     function(res){
+    //         console.log(res);
+    //     },
+    //     function(error){
+    //         console.log(error);
+    //     }
+    // );
 
     /*============ CONNECTION ============*/
+    app.get("/", function(req, res){
+        res.send("hello world");
+    });
+
     app.listen(3000, function(){
         console.log("Server is listening on port 3000");
     });
